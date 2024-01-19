27c5abbd2c5fa6ea751b9a0c036d0c98f7c9b7fa
garv <gsgojariya196003@gmail.com>
2024-01-19 17:05:42 +0530

    chats&image error solved

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
-import { Button } from '@mui/material'
 import { useFirebase } from '../context/Services'
 
 const ChatPage = () => {
